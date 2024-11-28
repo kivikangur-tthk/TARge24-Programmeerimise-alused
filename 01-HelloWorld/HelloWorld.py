f6e48aa0a99057f22b3578346206aebdb6958290
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2024-11-28 16:58:10 +0200

    Add function that prints "Hello world!"

--- 01-HelloWorld/HelloWorld.py
+++
@@ -1,1 +0,0 @@
-print("Hello World","Soovib Kristjan",sep="\n")
---
+++ 01-Input-Output-Errors/HelloWorld.py
@@ -0,0 +1,4 @@
+from Exercise_HelloWorld import print_hello_world
+
+print("Hello World","Soovib Kristjan",sep="\n")
+print_hello_world()
