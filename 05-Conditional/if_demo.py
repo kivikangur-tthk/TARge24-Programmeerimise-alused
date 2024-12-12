3f9b03a621c6cb8b13ec348fa8311e20d660d4cd
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2024-12-12 19:13:31 +0200

    Some conditional demos of automatic conversion

---
+++ 05-Conditional/if_demo.py
@@ -0,0 +1,12 @@
+a = True
+c = 5
+b = c >= 5
+
+if not a or not b or c:
+    print("Õnnestus")
+
+if print("Tere"):
+    print("Head aega")  # Ei käivitu kunagi
+
+if "Tere":
+    print("Tere Tere")
